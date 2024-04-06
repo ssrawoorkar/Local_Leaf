aca4472f6bad0a926951a39891eeca41ab955760
julgom <123409109+julgom@users.noreply.github.com>
2024-04-06 12:24:48 -0700

    Update FF.py

--- FF.py
+++ FF.py
@@ -1,4 +1,17 @@
-import urllib
+import urllibfrom geopy.geocoders import Photon
+ 
+# initialize Photon API
+geolocator = Photon(user_agent="geoapiExercises")
+ 
+ 
+# Latitude & Longitude input
+Latitude = "37.7764955"
+Longitude = "-122.4505532"
+ 
+location = geolocator.reverse(Latitude+","+Longitude)
+ 
 
+ 
+print(location)
 #https://www.gps-coordinates.net/my-location
 
